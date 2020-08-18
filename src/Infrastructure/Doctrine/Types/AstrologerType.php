94ef18bc99e22b9df25c627c917061d997c7ce84
sa <sa@netcraft.global>
2020-08-18 15:09:05 +0300

    Added doctrine types for Astrologers, Services. Moved doctrine mappings to Infrastructure

---
+++ src/Infrastructure/Doctrine/Types/AstrologerType.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Doctrine\Types;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\Type;
+
+class AstrologerType extends Type
+{
+    /**
+     * @param array $fieldDeclaration
+     * @param AbstractPlatform $platform
+     * @return string
+     */
+    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform): string
+    {
+        return $platform->getVarcharTypeDeclarationSQL($fieldDeclaration);
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return 'astrologer';
+    }
+}
---
+++ src/Migrations/Version20200818120508.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200818120508 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SEQUENCE astrologer_services_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE astrologers_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE services_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE users_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE astrologer_services (id UUID NOT NULL, astrologer VARCHAR(255) NOT NULL, service VARCHAR(255) NOT NULL, price NUMERIC(12, 2) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('COMMENT ON COLUMN astrologer_services.id IS \'(DC2Type:uuid)\'');
+        $this->addSql('CREATE TABLE astrologers (id UUID NOT NULL, name VARCHAR(255) NOT NULL, photo VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, description TEXT NOT NULL, personal_info TEXT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_81DB2C89E7927C74 ON astrologers (email)');
+        $this->addSql('COMMENT ON COLUMN astrologers.id IS \'(DC2Type:uuid)\'');
+        $this->addSql('CREATE TABLE services (id UUID NOT NULL, title VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('COMMENT ON COLUMN services.id IS \'(DC2Type:uuid)\'');
+        $this->addSql('CREATE TABLE users (id UUID NOT NULL, name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_1483A5E9E7927C74 ON users (email)');
+        $this->addSql('COMMENT ON COLUMN users.id IS \'(DC2Type:uuid)\'');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        $this->addSql('DROP SEQUENCE astrologer_services_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE astrologers_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE services_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE users_id_seq CASCADE');
+        $this->addSql('DROP TABLE astrologer_services');
+        $this->addSql('DROP TABLE astrologers');
+        $this->addSql('DROP TABLE services');
+        $this->addSql('DROP TABLE users');
+    }
+}
